c82e8d50ba92aa1e14e53eababd06b0798747f62
Tom Flanagan <tom@zoox.com>
2019-06-26 15:33:19 -0700

    stuff

--- bin/vm.sh
+++ bin/vm.sh
@@ -9,8 +9,11 @@ echo 1 | sudo tee /proc/sys/vm/vfs_cache_pressure
 # 50% of available memory to write cache
 echo 50 | sudo tee /proc/sys/vm/dirty_ratio
 
-# 30% of vailable memory to kernel write cache
-echo 30 | sudo tee /proc/sys/vm/dirty_background_ratio
+# start flushing dirty cache at 5%
+echo 5 | sudo tee /proc/sys/vm/dirty_background_ratio
+
+# 20s max dirty page time
+echo 20000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
 
 # don't kill shit at random
 echo 1 | sudo tee /proc/sys/vm/oom_kill_allocating_task
