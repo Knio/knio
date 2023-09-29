b2e08d547c4cb3a5f9faac02c008dc6f2d63ed62
Tom Flanagan <tom@zkpq.ca>
2023-09-28 22:41:58 -0700

    reorg

--- config/install/install.bat
+++ config/install/install.bat
@@ -1,10 +1,7 @@
-
-rem Run from repo root, ie ../knio> misc/install.bat
+rem Run from repo root, ie ../knio> config\install\install.bat
 rem Run from Administrator Command Prompt only (no PS)
 
 mklink "%APPDATA%\Code\User\settings.json" %cd%\config\Code\User\settings.json
 mklink "%APPDATA%\Code\User\keybindings.json" %cd%\config\Code\User\keybindings.json
 
 mklink %USERPROFILE%\.gitconfig %cd%\config\dotconfig\git\config
-
-
