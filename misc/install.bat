01a7b4524256d808011c8e19cacb2ee5c6be1b9f
Tom Flanagan <tom@zkpq.ca>
2023-09-28 22:01:58 -0700

    .

--- misc/install.bat
+++ misc/install.bat
@@ -2,9 +2,9 @@
 rem Run from repo root, ie ../knio> misc/install.bat
 rem Run from Administrator Command Prompt only (no PS)
 
-mklink "%APPDATA%\Code\User\settings.json" %cd%\.config\Code\User\settings.json
-mklink "%APPDATA%\Code\User\keybindings.json" %cd%\.config\Code\User\keybindings.json
+mklink "%APPDATA%\Code\User\settings.json" %cd%\config\Code\User\settings.json
+mklink "%APPDATA%\Code\User\keybindings.json" %cd%\config\Code\User\keybindings.json
 
-mklink %USERPROFILE%\.gitconfig %cd%\.config\git\config
+mklink %USERPROFILE%\.gitconfig %cd%\config\dotconfig\git\config
 
 
